4a3b2a358a116911ee2ddd86cacc534c74098d46
Quintero95 <quinteromeli18@gmail.com>
2021-09-15 19:30:54 -0500

    Clase Agenda

---
+++ Clase_Agenda.py
@@ -0,0 +1,6 @@
+# creamos nuestra clase agenda
+class Agenda:
+	# iniciamos nuestra clase
+	def __init__(self):
+		# crearemos una lista donde guardaremos los datos de nuestra agenda
+		self.contactos=[]

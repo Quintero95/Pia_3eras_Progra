198ec58f40fe6ac5061bb053a0296b72065c9647
Quintero95 <quinteromeli18@gmail.com>
2021-09-15 19:48:11 -0500

    Menu

---
+++ menu.py
@@ -0,0 +1,30 @@
+# menu del programa
+def menu(self):
+		print()
+		menu=[
+			['Agenda Personal'],
+			['1. Añadir Contacto',"anadir"],
+			['2. Lista de contactos'],
+			['3. Buscar contacto'],
+			['4. Editar contacto'],
+			['5. Cerrar agenda']
+		]
+ 
+		for x in range(6):
+			print(menu[x][0])
+ 
+		opcion=int(input("Introduzca la opción deseada: "))
+		if opcion==1:
+			self.anadir()
+		elif opcion==2:
+			self.lista()
+		elif opcion==3:
+			self.buscar()
+		elif opcion==4:
+			self.editar()
+		elif opcion==5:
+			print("Saliendo de la agenda ...")
+			exit()
+ 
+		# volvemos a llamar al menú
+		self.menu()
